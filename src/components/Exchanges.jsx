9f468f7a6dad229e46a3364414765634fb63b107
tej7272 <tej7272@gmail.com>
2023-08-28 15:05:56 +0530

    Cryptocoin

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import { Row, Col,
+  // Collapse, Typography, Avatar 
+} from 'antd';
 // import HTMLReactParser from 'html-react-parser';
 
 import { useGetExchangesQuery } from '../services/cryptoApi';
@@ -10,8 +12,10 @@ import Loader from './Loader';
 // const { Panel } = Collapse;
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetExchangesQuery();
-  const exchangesList = data?.data?.exchanges;
+  const { isFetching,
+    // data
+    } = useGetExchangesQuery();
+  // const exchangesList = data?.data?.exchanges;
 
  // Note: To access this endpoint you need premium plan
   if (isFetching) return <Loader />;
